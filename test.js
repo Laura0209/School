db08e24976f04958ee0487f118ab6bcc3b0e6184
Laura0209 <lautje_1993@hotmail.com>
2017-06-08 00:58:25 +0200

    Update files 02

---
+++ test.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+// var px = event.clientX;
+// var py = event.clientY;
+
+var draw = new Drawcircle();
+draw.init({obj: 'circle01', fill: '#ffff00'});
+
+// var draw = new pointer();
+// draw.circles('.circle01', {fill: '#343434'});
+
+// circle01({fill : "#343434"})
+
+// circle01.attr.fill = "#343434";
+
+// circle01.attr({fill: "#000000"});
+
+// init()
+
+// circle01.attr({obj: 'circle01',  fill: "#0000ff"});
+
+console.log(circle01.attr);
+// document.getElementsByTagName('BODY').onmousemove = function() {Drawcircle()};
+};
